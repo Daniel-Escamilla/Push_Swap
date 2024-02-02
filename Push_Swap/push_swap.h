f9e78ddbf2e3701af7e7747f98611335ab5a84eb
Descamil <73858568+Daniel404kali@users.noreply.github.com>
2024-02-02 18:03:31 +0100

    Add files via upload

---
+++ Push_Swap/push_swap.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 17:01:45 by descamil          #+#    #+#             */
+/*   Updated: 2024/02/02 17:12:55 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	int 			content;
+	struct s_list	*next;
+}					t_list;
+
+void	ft_error(void);
+int		ft_isdigit(int c);
+long	ft_atol(const char *str);
+char	**ft_split(const char *s, char c);
+
+t_list	*ft_lstnew(int content);
+void	ft_lstdelone(t_list *lst);
+void	ft_lstprinter(t_list *stack);
+void    ft_clear_stack(t_list **stack);
+int		ft_lstordered(t_list **stack_a);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **stack, t_list *new);
+
+void	ft_check_number(char *num);
+int		ft_duplicates(int number, t_list **stack_a);
+void	ft_new_node_number(int number, t_list **stack_a);
+void	ft_take_numbers(int argc, char **argv, t_list **stack_a);
+
+int	ft_lstsize(t_list *lst);
+void	ft_force(t_list **stack, int size);
+
+void	ft_swap(t_list	**stack, char *str);
+void	ft_rotate(t_list **stack,  char *str);
+void	ft_rrotate(t_list **stack,  char *str);
+void	ft_push(t_list **stack1, t_list **stack2, char *str);
+
+#endif
